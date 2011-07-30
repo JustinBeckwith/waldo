659f87d7489555a677c2592c01aa54cd58527a9e
Justin Beckwith <justin.beckwith@gmail.com>
2011-07-29 21:43:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waldo::Application.config.secret_token = 'b3975609dd1a6df1c99fc6c7300b1431bf106162a1f7cc1b315ab7157eb5502fbd27c1b2a2e31aa708ef895482ae99be1e2fabe8f0f2e077f50d9ddd62172313'
